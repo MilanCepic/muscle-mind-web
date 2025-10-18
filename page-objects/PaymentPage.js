9fa9861f0f0aa5611bd07230f14e6089db4be470
Milan Cepic <milancepic@gmail.com>
2025-10-18 17:19:58 +0200

    Fifth sreca

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -24,19 +24,14 @@ export class PaymentPage {
     if (!isDesktopViewport(this.page)) {
       await this.mobDetailsButton.waitFor({ state: "visible" });
       await this.mobDetailsButton.click();
-      await this.page.waitForTimeout(1000);
       await this.promotionCodeInputMobile.fill(code);
-      await this.applyPromotionButton.waitFor({ state: "visible" });
-      await this.applyPromotionButton.waitFor({ state: "stable" });
-      await this.applyPromotionButton.click({ force: true });
+      await this.applyPromotionButton.click();
       await this.page.waitForTimeout(1000);
       await this.mobDetailsButton.click();
       return;
     }
     await this.promotionCodeInput.fill(code);
-    await this.applyPromotionButton.waitFor({ state: "visible" });
-    await this.applyPromotionButton.waitFor({ state: "stable" });
-    await this.applyPromotionButton.click({ force: true });
+    await this.applyPromotionButton.click();
     await this.page.waitForTimeout(1000);
   };
   toggleSaveInfo = async () => {
